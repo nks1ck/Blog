35be5cd23cbbb6971eb684aa8ccf40d7e38ba7e5
nks1ck <nabiullin3553@gmail.com>
2020-08-20 21:00:56 +0300

    Add comments for articles

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -14,15 +14,14 @@ class PostListView(ListView):
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 поста на каждой странице
+    paginator = Paginator(object_list, 3)  # 3 posts in one page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # Если страница не является целым числом, поставим первую страницу
         posts = paginator.page(1)
     except EmptyPage:
-        # Если страница больше максимальной, доставить последнюю страницу результатов
+        # if page > pagemax, print last page
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
@@ -31,19 +30,26 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
 
-
-def post_share(request, post_id):
-    # Получить пост по id
-    post = get_object_or_404(Post, id=post_id, status='published')
+    comments = post.comments.filter(active=True)
+    new_comment = None
     if request.method == 'POST':
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
     else:
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+        comment_form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
+
