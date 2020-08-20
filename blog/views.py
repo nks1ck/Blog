1964787f13970c78a90f0e9a9dbe6364e9723757
nks1ck <nabiullin3553@gmail.com>
2020-08-20 23:36:56 +0300

    Tags filter and recommendations

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.db.models import Count
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import CommentForm
 
 
 class PostListView(ListView):
@@ -14,7 +15,7 @@ class PostListView(ListView):
 
 def post_list(request):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in one page
+    paginator = Paginator(object_list, 3)  # 3 post in one page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -46,10 +47,14 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
-
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
